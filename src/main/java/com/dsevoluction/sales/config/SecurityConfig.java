66e1791fc1aff041d993bda5697c8fa92e3703f2
douglas souza <douglas.souza1997@hotmail.com>
2022-09-29 15:35:40 -0300

    configuring urls

--- src/main/java/com/dsevoluction/sales/config/SecurityConfig.java
+++ src/main/java/com/dsevoluction/sales/config/SecurityConfig.java
@@ -29,6 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/clientes/**")
+                .authenticated()
+                .and()
+                .formLogin();
     }
 }
